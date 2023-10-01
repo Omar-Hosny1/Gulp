001525b4325320d9baf09521e22cb542ffcf82e1
Omar Hosny <102857390+Omar-Hosny1@users.noreply.github.com>
2023-10-02 00:42:30 +0300

    feat/added_some_tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,25 @@
 const gulp = require("gulp");
 const concat = require("gulp-concat");
+const prefix = require("gulp-autoprefixer");
 
 // First Task
 gulp.task("firstTask", async () => {
-    return gulp.src("project/*.*").pipe(gulp.dest("dist/copy"))
-})
-
+  return gulp.src("project/*.*").pipe(gulp.dest("dist/copy"));
+});
 
 // Second Task
-gulp.task("Concat_All_CSS_Files_To_Only_One_File_CSS", async () => {
-    return gulp.src("project/*.css")
-        .pipe(concat("main.css"))
-        .pipe(gulp.dest('dist'))
-})
+gulp.task("Concat_All_CSS_Files_To_Only_One_File", async () => {
+  return gulp
+    .src("project/*.css")
+    .pipe(concat("main.css"))
+    .pipe(gulp.dest("dist"));
+});
+
+// Third Task
+gulp.task("Concat_All_CSS_Files_To_Only_One_File_And_Prefixes", async () => {
+  return gulp
+    .src("./project/*.css")
+    .pipe(prefix())
+    .pipe(concat("main.css"))
+    .pipe(gulp.dest("./dist"));
+});
